2aab17dde4fb36205dd4863272a1b8bd9714ca9d
Pablo Gallastegui <pgallastegui@victoria.com>
2024-12-14 19:49:34 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Pablo Gallastegui on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
